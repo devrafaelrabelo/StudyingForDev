e8ab510e4bf160cfd8840c50b8b21aa769bd4a16
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-09-21 20:19:33 -0300

    3 Modulo - Aula 08 | Código

---
+++ Java/Java 13 Do Zero ao Profissional/3. Fundamentos/Aula_08/src/main.java
@@ -0,0 +1,40 @@
+
+/**
+ * Essa class representa .... JAVADOC
+ * @author Rafael Rabelo
+ * @since 1.0
+ */
+public class main {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Comentario de Uma Linha
+		//Comentario Linha simples		
+		System.out.println("//\n");//Comentario na frente do comando
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 */
+		System.out.println("*/\n"
+				+ "*\n"
+				+ "*/"); 
+		
+		/**
+		 * @author Rafael Rabelo
+		 */
+		System.out.println("*/\n"
+				+ "*\n"
+				+ "*/"); 
+
+
+	}
+
+}
