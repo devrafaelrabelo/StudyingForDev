2d37b40f5d98a95d44bc649fa3ff539c5eaf2d44
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-09-22 09:36:15 -0300

    3 Modulo - Aula 12 | Código

--- Java/Java 13 Do Zero ao Profissional/3. Fundamentos/Aula_12/src/telas/Inferencia.java
+++ Java/Java 13 Do Zero ao Profissional/3. Fundamentos/Aula_12/src/telas/Inferencia.java
@@ -6,10 +6,22 @@ public class Inferencia {
 		double a = 4.5;
 		System.out.println(a + " " + ((Object)a).getClass().getSimpleName());
 		
+		/*
+		 * VAR - Declara uma varial sem tipo, deixando para ser tipada no 
+		 * momento de inserir os dados mas nao pode ser alterado o tipo da
+		 * varial apos inicializar 
+		 */
 		var b = 4.5;
 		System.out.println(b + " " + ((Object)b).getClass().getSimpleName());
 		
 		var c = "4.5";
 		System.out.println(c + " " + ((Object)c).getClass().getSimpleName());
+		
+		/*	NÃO PODE SER DECLARADA DESSE JEITO
+		 * 	var c	
+		 * 	c = 145.00 ;	
+		 * 	System.out.println(c + " " + ((Object)c).getClass().getSimpleName());
+		 * 
+		 */
 	}
 }
