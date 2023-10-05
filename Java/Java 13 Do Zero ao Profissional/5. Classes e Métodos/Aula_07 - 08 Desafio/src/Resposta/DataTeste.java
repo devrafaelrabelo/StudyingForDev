584bb0b8ca510b693a967c9d17538e1eb650fb0a
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-10-04 21:45:45 -0300

    5 Modulo - Aula 07 - 08 Desafio | Código

---
+++ Java/Java 13 Do Zero ao Profissional/5. Classes e Métodos/Aula_07 - 08 Desafio/src/Resposta/DataTeste.java
@@ -0,0 +1,27 @@
+package Resposta;
+
+public class DataTeste {
+	
+	public static void main(String[] args) {
+		
+		Data niverRafael = new Data();
+		niverRafael.dia = 29;
+		niverRafael.mes = 03;
+		niverRafael.ano = 1995;
+		System.out.printf("Rafael Rabelo - %d/%d/%d", niverRafael.dia, niverRafael.mes, niverRafael.ano);
+	
+		System.out.println();
+		
+		var niverRoger = new Data();
+		niverRoger.dia = 17;
+		niverRoger.mes = 11;
+		niverRoger.ano = 1991;
+		System.out.printf("Roger Rabelo - %d/%d/%d", niverRoger.dia, niverRoger.mes, niverRoger.ano);
+		
+		
+		
+		
+	}
+	
+	
+}
