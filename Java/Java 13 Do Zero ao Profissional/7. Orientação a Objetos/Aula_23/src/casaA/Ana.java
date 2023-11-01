39216308176c8d48d9c4e2fb6a05a5b7390b9b50
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-11-01 10:16:28 -0300

    7 Modulo - Aula 23 | Com Código

---
+++ Java/Java 13 Do Zero ao Profissional/7. Orientação a Objetos/Aula_23/src/casaA/Ana.java
@@ -0,0 +1,11 @@
+package casaA;
+
+public class Ana {
+	
+	@SuppressWarnings("unused")
+	private String segredo = "Infiel";
+	String facoDentroDeCasa = "Lavo Roupa"; // default ou pacote
+	protected String formaDeFalar = "Grossa";
+	public String todosSabem = "Casada";
+	
+}
---
+++ Java/Java 13 Do Zero ao Profissional/7. Orientação a Objetos/Aula_23/src/casaB/Pedro.java
@@ -0,0 +1,16 @@
+package casaB;
+
+import casaA.Ana;
+
+public class Pedro extends Ana{
+	
+	
+	void testeAcessos() {
+		
+//		System.out.println(segredo); //  Algo Privado
+//		System.out.println(facoDentroDeCasa); 
+		System.out.println(formaDeFalar);
+		System.out.println(new Ana().todosSabem);
+		
+	}
+}
