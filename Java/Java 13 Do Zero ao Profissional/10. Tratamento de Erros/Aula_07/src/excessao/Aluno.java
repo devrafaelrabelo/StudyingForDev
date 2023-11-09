5b8a3e80ba4c35616eee16815df206cec5d7bedf
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-11-09 17:26:43 -0300

    10 Módulo - Aula 07 | Com Código

---
+++ Java/Java 13 Do Zero ao Profissional/10. Tratamento de Erros/Aula_07/src/excessao/Aluno.java
@@ -0,0 +1,19 @@
+package excessao;
+
+public class Aluno {
+
+	public String nome;
+	public double nota;
+	public boolean bomCoportamento;
+
+	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+
+	public Aluno(String nome, double nota, boolean bomCoportamento) {
+		this.nome = nome;
+		this.nota = nota;
+		this.bomCoportamento = bomCoportamento;
+	}
+
+}
