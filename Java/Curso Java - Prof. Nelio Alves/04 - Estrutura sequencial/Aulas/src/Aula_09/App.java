66f61ccff3dc3cb0482d4cd4440dd898baeca979
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-11-12 19:28:10 -0300

    4 Módulo - Aula 09

--- Java/Curso Java - Prof. Nelio Alves/04 - Estrutura sequencial/Aulas/src/Aula_09/App.java
+++ Java/Curso Java - Prof. Nelio Alves/04 - Estrutura sequencial/Aulas/src/Aula_09/App.java
@@ -17,14 +17,14 @@ public class App {
 
         int x = sc.nextInt();
         sc.nextLine();
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        String s3 = sc.nextLine();
+        String s4 = sc.nextLine();
+        String s5 = sc.nextLine();
+        String s6 = sc.nextLine();
 
         System.out.println(x);
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
 
 
         sc.close();
