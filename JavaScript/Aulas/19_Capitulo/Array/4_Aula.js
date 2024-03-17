b76dd33932e77626b1b0d2266b1d6641835bd6f2
Rafael Rabelo <devrafaelrabelo@gmail.com>
2024-03-17 18:07:27 -0300

    feat: Cpitulo 19 - part 1

---
+++ JavaScript/Aulas/19_Capitulo/Array/4_Aula.js
@@ -0,0 +1,48 @@
+var signos = [
+    "Áries",
+    "Touro",
+    "Gêmeos",
+    "Câncer",
+    "Leão",
+    "Virgem",
+    "Libra",
+    "Escorpião",
+    "Sagitário",
+    "Capricórnio",
+    "Aquário",
+    "Peixes"
+];
+
+console.log(signos.length); // 12
+
+// -----------------------------------------------
+
+var times_rj = [
+    "Botafogo",
+    "Flamengo",
+    "Fluminense",
+    "Vasco",
+    "Volta Redonda"
+];
+
+var total_times_rj = times_rj.length;
+
+console.log("Total de Times: " + total_times_rj);
+
+// Total de Times: 5
+
+
+var times_sp = [
+    "Bragantino",
+    "Corinthians",
+    "Palmeiras",
+    "Ponte Preta",
+    "São Paulo",
+    "Santos"
+];
+
+var total_times_sp = times_sp.length;
+
+console.log("Total de Times: " + total_times_sp);
+
+// Total de Times: 6
