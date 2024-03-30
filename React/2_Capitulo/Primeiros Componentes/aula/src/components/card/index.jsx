a2b7776311ad1a0a39085ef274a803eef5a063bb
devrafaelrabelo <devrafaelrabelo@gmail.com>
2024-03-30 11:27:47 -0300

    feat: Finalizando - 2 Capitulo - 2 Aula

---
+++ React/2_Capitulo/Primeiros Componentes/aula/src/components/card/index.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+  return (
+    <div className={styles.card}>
+      <h1>Hello World</h1>
+      <p>Esse é o nosso primeiro componente</p>
+      <p>Componente criado com React.js</p>
+    </div>
+  );
+}
